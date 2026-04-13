f31ff5c15187e8f17c5463032ea4f3d3983b78e9
dziedziedziek <dziedziedziek@gmail.com>
2026-04-13 10:44:46 +0200

    Add files via upload

---
+++ kick.lua
@@ -0,0 +1,19 @@
+﻿local Players = game:GetService("Players")
+
+-- Funkcja wykrywająca czat
+local function onPlayerAdded(player)
+	player.Chatted:Connect(function(message)
+		if message:lower() == ".k" then
+			-- wyrzuca gracza który napisał .k
+			player:Kick("Unknown Error.")
+		end
+	end)
+end
+
+-- dla nowych graczy
+Players.PlayerAdded:Connect(onPlayerAdded)
+
+-- dla tych co już są na serwerze
+for _, player in pairs(Players:GetPlayers()) do
+	onPlayerAdded(player)
+end
